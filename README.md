045eb98b1b1f7b86006845ee7acc7a1d0c7d8a1a
Raka Rendra Fayanto <l200224246@student.ums.ac.id>
2024-09-22 14:33:16 +0700

    Assignment Week-1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repository for Framework Web Progamming
+
+## Raka Rendra Fayanto
+## L200224246
+## Universitas Muhammadiyah Surakarta
--- mysite/polls/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return HttpResponse("Hello World! from polls view   ")
-# Create your views here.
